35372784c7ca52ae22eb8d31500f0140b61a47ab
Tobiad <oluwatobichukwuebuka@gmail.com>
2022-09-14 14:54:38 -0700

    Functions Nested Loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include<main.h>
+
+int main(void)
+	/** A command to print _putchar followed by a new line
+	 *
+	 * declare putchar
+	 * use ASCII method
+	 * Return 0 when done
+	 */
+{
+putchar(95,80,85,84,67,72,65,82,'\n');
+Return(0)
+}
