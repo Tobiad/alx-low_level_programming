dfd4df16803ee27b6b6f500a0a0045faca4216eb
Tobiad <oluwatobichukwuebuka@gmail.com>
2022-09-14 15:27:37 -0700

    Functions Nested Loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,6 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-/**
- * _putchar - writes the character c to std
- * @c: parameter to compare
- * Return: 1 or 0
- */
+#ifndef main_h
+#define main_h
+
 int _putchar(char c);
-{
-	return (write(1, &c, 1));
-}
+
+#endif
