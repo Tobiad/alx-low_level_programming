7e5a9434571bb0796ca4d00fd79931e239901706
Tobiad <oluwatobichukwuebuka@gmail.com>
2022-09-19 21:58:36 -0700

    Pointers, arrays and string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,14 +2,5 @@
 #define main_h
 
 void reset_to_98(int *n);
-int _putchar(char);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts_half(char *str);
-void puts2(char *str);
-void print_array(int *a, int n);
-int _atoi(char *s);
-char *_strcpy(char *dest, char *src);
+
+#endif
