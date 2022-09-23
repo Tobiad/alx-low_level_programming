8247f6d1ae9e603d156e974a772b657619ff6412
Tobiad <oluwatobichukwuebuka@gmail.com>
2022-09-23 16:20:19 -0700

    Pointers, arrays and string

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,20 +2,20 @@
 
 int main(void)
 {
-	/**
-	 * main - Entry point
-	 * a is not allowed
-	 * p variable should not be modified
-	 * , is not allowed
-	 * code should be written at line 19 before ;
-	 * Do not remove anything not even comments
-	 */
 int n;
 int a[5];
 int *p;
 
 a[2] = 1024;
 p = &n;
+/*
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
+*/
 p[5] = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
