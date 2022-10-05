01aca69a59afa6f231c33bc03940cdc2a42f9abf
Tobiad <oluwatobichukwuebuka@gmail.com>
2022-10-05 13:54:47 -0700

    argc argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - programme that prints number of args
+ * @argv: vector variable
+ * @argc: variable counts
+ * Return: 0 - success
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argv;
+	printf("argc = %d\n", argc);
+	return (0);
+}
