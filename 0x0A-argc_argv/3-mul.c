250651eb1f670692826a2a579fda04b63e6dd427
Tobiad <oluwatobichukwuebuka@gmail.com>
2022-10-05 21:53:32 -0700

    argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,4 +9,12 @@
  */
 
 int main(int argc, char *argv[])
-
+{
+	(void) argc;
+	if (argc != 3)
+	{	printf("Error\n");
+		return (1);
+	}
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
