8ed51fdb75ba9782dbfeb31028f7180d770dc36a
Tobiad <oluwatobichukwuebuka@gmail.com>
2022-10-05 21:36:21 -0700

    argc, argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a programme that multiplies
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: 0 - success
+ */
+
+int main(int argc, char *argv[])
+
