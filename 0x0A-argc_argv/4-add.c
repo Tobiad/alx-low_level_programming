e35d316490b342827f1136174ff616ae67533342
Tobiad <oluwatobichukwuebuka@gmail.com>
2022-10-05 21:59:30 -0700

    argc, argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - funtions that add
+ * @argc: arguement count
+ * @argv: arguement vector
+ * return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	
+}
