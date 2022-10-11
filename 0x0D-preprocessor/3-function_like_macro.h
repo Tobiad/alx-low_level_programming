86ae5fccc35b165a1b230a0cea9ce4ffbfd6e9be
Tobiad <oluwatobichukwuebuka@gmail.com>
2022-10-11 15:33:29 -0700

    preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef _function_like_macro_h_
-#define _function_like_macro_h_
-#define ABS(x) (((x) < (0))) ? ((x) * (-1)) : (x))
-#endif /* _function_like_macro_h_ i*/
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+
+#define ABS(x) ((x) < (0) ? (-(x)) : (x))
+
+#endif
