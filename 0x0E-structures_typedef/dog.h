5c4f78af8071167581febc58ce27f2deddb677b2
Tobiad <oluwatobichukwuebuka@gmail.com>
2022-10-11 21:37:18 -0700

    structs and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,9 +20,9 @@ typedef struct dog dog_t;
  struct dog
 {
 char *name;
-float age; 
+float age;
 char *owner;
-}; 
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
