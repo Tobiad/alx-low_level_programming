4755bb604fb35670808c4d28f30cc35ca2f28e46
Tobiad <oluwatobichukwuebuka@gmail.com>
2022-10-11 21:50:07 -0700

    alx

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,9 +15,9 @@ typedef struct dog dog_t;
  * @owner: owner of the dog
  *
  * Description: struct called "dog" that stores its name, its age
- * and the name of its owner.
+ * and the name of its owner.<F11>
  */
- struct dog
+struct dog
 {
 char *name;
 float age;
